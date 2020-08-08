4a3484a99437ce60e10953a9b7d45696e088620b
Ramkumar Sundarajan Meenakshi <ramkumar.sundarajan.meenakshi@oracle.com>
2020-08-07 23:43:54 -0400

    updated code to download image from url

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 const cors = require('cors');
 const port =  3000
 const bodyParser = require('body-parser')
+const fs = require('fs')
+const request = require('request')
 
 
 app.use(express.static('public'))
@@ -16,31 +18,54 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+const download = (url, path, callback) => {
+    request.head(url, (err, res, body) => {
+      request(url)
+        .pipe(fs.createWriteStream(path))
+        .on('close', callback)
+    })
+  }
+
+//   image URL 
+const image_url = 'https://ibb.co/QKPDmzh'
+
+const path = './images/image_scan.png'
+
+const post_download_callback = () => {
+        console.log('Image download complete!')
+        console.log('Recognizing Downloaded Image ....');
+
+        Tesseract.recognize(image, 'eng', { logger: m => console.log(m) })
+        .then(({ data: { text } }) => {
+            console.log(text);
+            res.send(`Scanned Text = ${text}`);
+        });
+}
+
 app.post('/api/scan', function(req, res) {
 
-    var user_id = req.body.id;
+    var myimage = req.body.imagepath;
     var token = req.body.token;
     var geo = req.body.geo;
 
+    download(image_url, path, post_download_callback())
+
+    // const [,, imagePath] = process.argv;
+    // const image = path.resolve(__dirname, (myimage || 'images/images.png'));
+
+    // console.log(`Recognizing ${image}`);
 
 
-    const [,, imagePath] = process.argv;
-    const image = path.resolve(__dirname, (imagePath || 'images/images.png'));
+    
 
-    console.log(`Recognizing ${image}`);
-
-
-    Tesseract.recognize(image, 'eng', { logger: m => console.log(m) })
-        .then(({ data: { text } }) => {
-            console.log(text);
-    });
-
-    res.send(`welcome `);
+    
 
 });
 
 app.listen(port, () => console.log(`OCR app listening on port ${port}!`))
 
+
+ 
 
 
 
